d0bbeb203228ee7bdae8bef2ae7f79aada6d6c70
Celine Bouhafs <celine.b.dev@gmail.com>
2024-11-12 11:48:28 +0100

    Mobile LandingPage & Nav
    - Nav adjustments
    - Home Page visual

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,6 @@
    <nav class="navbar">
       
      <div class="navbar__icon">
-       <!-- Menu icon stays visible, but will be hidden under the sliding menu -->
        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48"><g fill="none" fill-rule="evenodd"><circle cx="24" cy="24" r="24" fill="#FFF"/><path fill="#0B0D17" d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z"/></g></svg>
        <div
          class="navbar__icon__menu"
@@ -15,12 +14,9 @@
          </svg>
        </div>
      </div>
-     <!-- Menu list with animation -->
      <div class="navbar__list-wrapper" :class="{ 'navbar__list-wrapper--open': isMenuOpen }">
        <ul class="navbar__list">
-         <!-- Close icon now follows the list -->
          <div class="navbar__icon__close-wrapper">
-           <!-- Close icon shown when menu is open -->
            <div class="navbar__icon__close" @click="toggleMenu">
              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="21">
                <g fill="#D0D6F9" fill-rule="evenodd">
@@ -61,6 +57,14 @@
 
 <style scoped lang="scss">
 .navbar {
+  padding: 25px 22px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background-color: transparent;
+  z-index: 100;
+  
   &__icon {
     display: flex;
     align-items: center;
@@ -68,6 +72,7 @@
     position: relative;
     z-index: 5;
     font-family: 'Cantarell';
+
     &__menu {
       cursor: pointer;
       font-size: 1.5rem;
@@ -79,10 +84,11 @@
     position: fixed;
     top: 0;
     right: -100%;
-    width: 65%;
+    width: 67%;
     height: 100vh;
     z-index: 10;
-    transition: right 1.7s cubic-bezier(0.25, 0.8, 0.25, 1);
+    transition: right 1.3s cubic-bezier(0.25, 0.8, 0.25, 1);
+    backdrop-filter: blur(35px);
     
     &--open {
       right: 0;
@@ -91,27 +97,26 @@
     .navbar__list {
       list-style: none;
       height: 100%;
-      background-color: grey;
-      padding: 100px 0 0 30px;
+      padding: 122px 0 0 30px;
+
       &__item-number {
-      font-weight: bold;
-      margin-right: 10px;
-    }
-    &__item {
-      margin-top: 18px;
-      font-family: 'Barlow Condensed', sans-serif;
-      font-size: 16px;
-      letter-spacing: 2px;
-    }
+        font-weight: bold;
+        margin-right: 10px;
+      }
 
+      &__item {
+        margin-top: 24px;
+        font-family: 'Barlow Condensed', sans-serif;
+        font-size: 16px;
+        letter-spacing: 2px;
+        color: white;
+      }
     }
-
-
 
     .navbar__icon__close-wrapper {
       position: absolute;
-      top: 25px;
-      right: 35px;
+      top: 30px;
+      right: 29px;
       z-index: 15;
     }
 
@@ -123,149 +128,3 @@
 }
 </style>
 
-
- 
- 
- 
- 
- 
- 
- 
- 
- 
-
-
-
-
- 
-
-
- <!-- <template>
-   <nav class="navbar">
-     <div class="navbar__icon">
-       <div class="navbar__icon__logo">
-         <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48">
-           <g fill="none" fill-rule="evenodd">
-             <circle cx="24" cy="24" r="24" fill="#FFF" />
-             <path fill="#0B0D17" d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z" />
-           </g>
-         </svg>
-       </div>
-       <div class="navbar__icon__menu" @click="toggleMenu" v-if="!isMenuOpen">
-         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="21">
-           <g fill="#D0D6F9" fill-rule="evenodd">
-             <path d="M0 0h24v3H0zM0 9h24v3H0zM0 18h24v3H0z" />
-           </g>
-         </svg>
-       </div>
-       <div class="navbar__icon__close" @click="toggleMenu" v-if="isMenuOpen">
-         <svg xmlns="http://www.w3.org/2000/svg" width="20" height="21">
-           <g fill="#D0D6F9" fill-rule="evenodd">
-             <path d="M2.575.954l16.97 16.97-2.12 2.122L.455 3.076z" />
-             <path d="M.454 17.925L17.424.955l2.122 2.12-16.97 16.97z" />
-           </g>
-         </svg>
-       </div>
-     </div>
-      Utilisation de v-if pour la gestion du DOM et des transitions
-     <ul v-if="isMenuOpen" class="navbar__list" :class="{ 'navbar__list--open': isMenuOpen }">
-       <li class="navbar__list__item">
-         <router-link to="/" aria-label="Home">HOME</router-link>
-       </li>
-       <li class="navbar__list__item">
-         <router-link to="/destination" aria-label="Destination">DESTINATION</router-link>
-       </li>
-       <li class="navbar__list__item">
-         <router-link to="/crew" aria-label="Crew">CREW</router-link>
-       </li>
-       <li class="navbar__list__item">
-         <router-link to="/technology" aria-label="Technology">TECHNOLOGY</router-link>
-       </li>
-     </ul>
-   </nav>
- </template>
- 
- <script lang="ts">
- import { defineComponent, ref } from 'vue';
- 
- export default defineComponent({
-   name: 'NavBar',
-   setup() {
-     const isMenuOpen = ref(false);
- 
-     const toggleMenu = () => {
-       isMenuOpen.value = !isMenuOpen.value;
-     };
- 
-     return {
-       isMenuOpen,
-       toggleMenu,
-     };
-   },
- });
- </script>
-
- 
- <style scoped lang="scss">
- .navbar {
-   &__icon {
-     display: flex;
-     align-items: center;
-     justify-content: space-between;
- 
-     &__logo {}
- 
-     &__menu,
-     &__close {
-       cursor: pointer;
-       display: block;
-     }
- 
-     &__close {
-       z-index: 6;
-     }
-   }
- 
-   &__list {
-     position: fixed;
-     top: 0;
-     left: 100%; /* Par défaut, la div est complètement à droite */
-     list-style: none;
-     justify-content: flex-end;
-     height: 100vh;
-     width: 100%;
-     background-color: grey;
-     z-index: 5;
-     padding: 75px 0 0 35px;
-     transition: transform 0.5s ease-in-out; /* Transition sur transform */
- 
-     /* Transition vers la gauche quand le menu est ouvert */
-     &--open {
-       transform: translateX(-100%); /* Déplacement à gauche */
-     }
- 
-     &__item {
-       margin-top: 25px;
- 
-       a {
-         color: white;
-         text-decoration: none;
-         transition: color 0.3s;
- 
-         &:hover {
-           color: #ff6f61;
-         }
-       }
-     }
-   }
- 
-   &__icon__close {
-     display: block;
-     z-index: 6;
-   }
- 
-   &__icon__menu {
-     display: block;
-   }
- }
- </style> -->
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,122 @@
-
-
 <script setup lang="ts">
 </script>
 
 <template>
   <main>
-    <header>
+    <header class="intro">
+      <div class="intro__first-text">
+        <p>SO, YOU WANT TO TRAVEL TO</p>
+      </div>
+      <div class="intro__title">
+        <h1>SPACE</h1>
+      </div>
+      <div class="intro__second-text">
+        <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+      </div>
     </header>
+    <section class="btn">
+      <button class="btn__explore">
+      <p>EXPLORE</p>
+    </button>
+    </section>
+
   </main>
 </template>
+
+<style scoped lang="scss">
+main {
+  background-image: url('@/assets/home/background-home-mobile.jpg');
+  background-size: cover;
+  background-position:center;
+  background-repeat: no-repeat;
+  min-height: 100vh;
+}
+
+.intro {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 510px;
+  width: 100%;
+  row-gap: 5px;
+
+  &__first-text {
+    color: var(--vt-c-white-blue);
+    font-family: 'Barlow Condensed';
+    font-size: 1rem;
+    letter-spacing: 2px;
+  }
+
+  &__title {
+    color: var(--vt-c-white);
+    font-family: "Bellefair", serif;
+    font-size: 2.4rem;
+  }
+
+  &__second-text {
+    padding: 0 35px;
+    text-align: center;
+    color: var(--vt-c-white-blue);
+    font-size: 0.9rem;
+    font-family: 'Barlow Condensed';
+    letter-spacing: 1px;
+  }
+}
+
+.btn {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 130px;
+  &__explore {
+  position: relative; /* Nécessaire pour positionner le pseudo-élément */
+  background-color: var(--vt-c-white);
+  height: 144px;
+  width: 144px;
+  border-radius: 100%;
+  border-style: none;
+  font-family: 'Bellefair';
+  font-size: 1.1rem;
+  cursor: pointer;
+  z-index: 10;
+
+  /* Pseudo-élément pour le cercle */
+  &::after {
+    content: ''; /* Nécessaire pour générer le pseudo-élément */
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 220%; /* Le cercle est plus grand que le bouton */
+    height: 220%; /* Le cercle est plus grand que le bouton */
+    background-color: #FFFFFF; /* Couleur explicite du cercle */
+    border-radius: 50%; /* Cercle parfait */
+    transform: translate(-50%, -50%); /* Centrer le cercle */
+    opacity: 0; /* Le cercle est invisible au départ */
+    z-index: 1; /* Le cercle est derrière le texte du bouton */
+    transition: opacity 0.4s ease; /* Simple fondu */
+  }
+
+  /* Effet hover sur le bouton uniquement */
+  &:hover::after {
+    opacity: 0.1; /* Le cercle devient visible */
+  }
+}
+}
+
+
+
+
+
+
+
+
+</style>
+
+
+
+
+
 
 <!-- <style lang="scss" module src="../styles/HomeView.module.scss"></style> -->
 
